bb1f19ee484624916807315c0d013246d92e2bc8
tahsinAmin <muhammadtahsinamin@gmail.com>
2025-07-10 23:51:37 +0600

    impr (item): - Each item has a description, status & id along with title - Form input UI adjustment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,38 +2,48 @@
 
 import { useState } from "react"
 
+type taskProp = {
+  id: string,
+  title: string,
+  status: string,
+  description: string
+}
+
 export default function Home() {
-  const [newTask, setNewTask] = useState<string[]>(["new 1", "new 2", "new 3"])
-  const [ongoing, setOngoing] = useState<string[]>(["ongoing 1", "ongoing 2", "ongoing 3"])
-  const [done, setDone] = useState<string[]>(["done 1", "done 2", "done 3"])
+  const [newTask, setNewTask] = useState<taskProp[]>([{id: "1", title:"new 1", status:"new", description:"new 1"}, {id: "2", title:"new 2", status:"new", description:"new 2"}, {id: "3", title:"new 3", status:"new", description:"new 3"}])
+  const [ongoing, setOngoing] = useState<taskProp[]>([{id: "4", title:"ongoing 1", status:"ongoing", description:"ongoing 1"}, {id: "5", title:"ongoing 2", status:"ongoing", description:"ongoing 2"}, {id: "6", title:"ongoing 3", status:"ongoing", description:"ongoing 3"}])
+  const [done, setDone] = useState<taskProp[]>([{id: "7", title:"done 1", status:"done", description:"done 1"}, {id: "8", title:"done 2", status:"done", description:"done 2"}, {id: "9", title:"done 3", status:"done", description:"done 3"}])
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    console.log(e.currentTarget.task.value)
-    setNewTask([e.currentTarget.task.value, ...newTask])
+    let newItem = { id: crypto.randomUUID(), title: e.currentTarget.task.value, status: "new", description: e.currentTarget.description.value };
+    setNewTask([newItem, ...newTask])
     e.currentTarget.task.value = ""
+    e.currentTarget.description.value = ""
   }
 
-  const moveToOngoing = (task: string) => {
+  const moveToOngoing = (task: taskProp) => {
     setOngoing([...ongoing, task])
-    setNewTask(newTask.filter((newTask) => newTask !== task))
+    setNewTask(newTask.filter((newTask) => newTask.title !== task.title))
   }
 
-  const moveToDone = (task: string) => {
+  const moveToDone = (task: taskProp) => {
     setDone([...done, task])
-    setOngoing(ongoing.filter((ongoing) => ongoing !== task))
+    setOngoing(ongoing.filter((ongoing) => ongoing.title !== task.title))
   }
 
-  const removeDone = (task: string) => {
-    setDone(done.filter((done) => done !== task))
+  const removeDone = (task: taskProp) => {
+    setDone(done.filter((done) => done.title !== task.title))
   }
 
   return (
     <div className="container mx-auto">
       <div className="flex flex-col gap-4 p-4">
 
-        <form onSubmit={handleSubmit}>
-          <input type="text" name="task" className="border border-gray-300 rounded p-2" />
+        <form onSubmit={handleSubmit} className="flex flex-col gap-2 border border-4 border-gray-300 rounded-3xl p-4 w-1/2">
+          <div className="tag border border-gray-300 rounded-full p-2 display-inline w-fit">New Task</div>
+          <input placeholder="Enter title" name="task" className="border border-gray-300 rounded p-2" />
+          <textarea placeholder="Enter description" name="description" className="border border-gray-300 rounded p-2" />
           <input type="submit" value="Add" className="border border-gray-300 rounded p-2"/>
         </form>
 
@@ -44,7 +54,7 @@ export default function Home() {
               <h2 className="text-2xl font-bold">Todo</h2>
               <ul className="flex flex-col gap-2 border border-gray-300 rounded p-2">
                 {newTask.map((newTask) => (
-                  <li key={newTask} className="text-xl">{newTask} <button onClick={() => moveToOngoing(newTask)}>Move</button></li>
+                  <li key={newTask.id} className="text-xl">{newTask.title} <button onClick={() => moveToOngoing(newTask)}>Move</button></li>
                 ))}
               </ul>
             </div>
@@ -52,7 +62,7 @@ export default function Home() {
               <h2 className="text-2xl font-bold">Ongoing</h2>
               <ul className="flex flex-col gap-2 border border-gray-300 rounded p-2">
                 {ongoing.map((ongoing) => (
-                  <li key={ongoing} className="text-xl">{ongoing} <button onClick={() => moveToDone(ongoing)}>Move</button></li>
+                  <li key={ongoing.id} className="text-xl">{ongoing.title} <button onClick={() => moveToDone(ongoing)}>Move</button></li>
                 ))}
               </ul>
             </div>
@@ -60,7 +70,7 @@ export default function Home() {
               <h2 className="text-2xl font-bold">Done</h2>
               <ul className="flex flex-col gap-2 border border-gray-300 rounded p-2">
                 {done.map((done) => (
-                  <li key={done} className="text-xl">{done} <button onClick={() => removeDone(done)}>Move</button></li>
+                  <li key={done.id} className="text-xl">{done.title} <button onClick={() => removeDone(done)}>Remove</button></li>
                 ))}
               </ul>
             </div>
