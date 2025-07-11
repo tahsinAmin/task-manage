abcb41077299589c9c6b593d77ffadda0f78d674
tahsinAmin <muhammadtahsinamin@gmail.com>
2025-07-11 08:29:34 +0600

    feat (item): Show Description and Status in item

--- src/app/components/Board.tsx
+++ src/app/components/Board.tsx
@@ -7,7 +7,12 @@ const Board = ({title, tasks, state, shiftTask}: {title: string, tasks: any, sta
               <h2 className="text-2xl font-bold">{title}</h2>
               <ul className="flex flex-col gap-2 border border-gray-300 rounded p-2">
                 {tasks.map((task: taskProp) => (
-                  <li key={task.id} className="text-xl">{task.title} <button onClick={() => shiftTask(state, task)}>Move</button></li>
+                  <li key={task.id} className="flex flex-col gap-2 border border-4 border-gray-300 rounded-3xl p-4">
+                    <div className="tag border border-gray-300 rounded-full p-2 display-inline w-fit">{task.status}</div>
+                    <div className="text-xl">{task.title}</div>
+                    <div>{task.description}</div>
+                    <button onClick={() => shiftTask(state, task)}>Move</button>
+                  </li>
                 ))}
               </ul>
             </div>
