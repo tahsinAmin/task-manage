255857f378ac7f48600c75972c7d748b1ba37d17
tahsinAmin <muhammadtahsinamin@gmail.com>
2025-07-12 01:19:27 +0600

    impr (form): title required

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,7 +54,7 @@ export default function Home() {
         <div className="flex justify-center">
           <form onSubmit={handleSubmit} className="flex flex-col gap-2 border border-4 border-gray-300 rounded-3xl p-4 sm:w-1/2">
             <div className="tag border border-gray-300 rounded-full p-2 display-inline w-fit">New Task</div>
-            <input placeholder="Enter title" name="task" className="border border-gray-300 rounded p-2" />
+            <input required placeholder="Enter title" name="task" className="border border-gray-300 rounded p-2" />
             <textarea placeholder="Enter description" name="description" className="border border-gray-300 rounded p-2" />
             <input type="date" name="dueDate" className="border border-gray-300 rounded p-2" />
             <input type="submit" value="Add" className="border border-gray-300 rounded p-2" />
